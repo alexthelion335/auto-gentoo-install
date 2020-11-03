587a28becd6ebc699c4b9b228675da910ad97d03
Alex Kinch <alexthelion335@gmail.com>
2020-11-03 17:19:43 -0500

    Update install-arch2.sh

--- install-arch2.sh
+++ install-arch2.sh
@@ -8,9 +8,9 @@ ln -sf /usr/share/zoneinfo/America/New_York
 hwclock --systohc
 echo -e "\nInstalling other necessities..."
 pacman -S nano vim sudo base-devel git networkmanager dhcpcd grub xorg-server xf86-video-vesa xorg-xinit neofetch i3-gaps i3lock i3status feh firefox konsole xterm mpv thunar dolphin picom inkscape gimp cmatrix lynx lolcat cowsay << EOF
-  
-  
-  
+
+
+
 EOF
 echo -e "\nOther necessities installed!"
 echo $hostname >> /etc/hostname
@@ -18,13 +18,13 @@ rm /etc/hosts
 echo -e "127.0.0.1    localhost\n::1        localhost\n127.0.1.1    ${hostname}.localdomain ${hostname}" >> /etc/hosts
 echo -e "\nHostname set!"
 passwd << EOF
-  $passwd
-  $passwd
+$passwd
+$passwd
 EOF
 useradd -m $user
 passwd $user << EOF
-  $passwd
-  $passwd
+$passwd
+$passwd
 EOF
 usermod -aG wheel,audio,video,optical,storage $user
 printf "\n%%wheel ALL=(ALL) ALL" >> /etc/sudoers
