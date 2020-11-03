7157e5458b6bd1df494474353e023454f81e51a9
Alex Kinch <alexthelion335@gmail.com>
2020-11-03 17:37:02 -0500

    Update install-arch2.sh

--- install-arch2.sh
+++ install-arch2.sh
@@ -6,9 +6,6 @@ passwd=password
 echo -e "\nContinuing installation..."
 ln -sf /usr/share/zoneinfo/America/New_York
 hwclock --systohc
-echo -e "\nInstalling other necessities..."
-echo -e "y\n" >> pacman -S nano vim sudo base-devel git networkmanager dhcpcd grub xorg-server xf86-video-vesa xorg-xinit neofetch i3-gaps i3lock i3status openbox tint2 nitrogen firefox konsole xterm mpv thunar dolphin picom inkscape gimp cmatrix lynx lolcat cowsay
-echo -e "\nOther necessities installed!"
 echo $hostname >> /etc/hostname
 rm /etc/hosts
 echo -e "127.0.0.1    localhost\n::1        localhost\n127.0.1.1    ${hostname}.localdomain ${hostname}" >> /etc/hosts
